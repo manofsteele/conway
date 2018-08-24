1ca415f22505aa9f43f1a0e65267404f9b5e9d08
Jeremiah Steele <jeremiahsteele@gmail.com>
2018-08-23 20:15:47 -0700

    Make submit button reset board too

--- conway.js
+++ conway.js
@@ -17,13 +17,14 @@
 // let percentage = 50;
 // let timer;
 //
+let board;
 
 function createBoard() {
   const grid = new Array(60);
   for (let i = 0; i < grid.length; i++) {
     grid[i] = new Array(60);
   }
-  const board = {
+  board = {
     grid: grid,
     numLiving: 0,
     numGenerations: 0,
@@ -54,7 +55,6 @@ function updateBoard(board) {
 
 let lastNumLiving = board.numLiving;
 board.numLiving = 0;
-// console.log("update running");
 const nextBoard = createBoard();
 
 let neighbors = [];
@@ -144,7 +144,7 @@ function renderBoard(board) {
     html +=`<br/>`;
   }
   html +=`<br/>`;
-  let boardDiv = document.getElementById("board");
+  let boardDiv = document.getElementById("boardGrid");
   boardDiv.innerHTML = html;
   html = "";
   html +=`<div class="stats">Number of living cells: ${board.numLiving}</br>`;
@@ -158,7 +158,7 @@ function renderBoard(board) {
 
 
 
-function populateCrazyBoard(board) {
+function populateAcornBoard(board) {
   for (let i = 0; i < board.grid.length; i++) {
     for (let j = 0; j < board.grid.length; j++) {
       board.grid[i][j] = 0;
@@ -196,6 +196,8 @@ function handlePercentageInput() {
   let input = document.getElementById("percentageInput").value;
   if (input >= 0 && input <= 100) {
     board.percentage = input;
+    stopBoard(board);
+    resetBoard(board);
   }
 }
 
@@ -203,7 +205,7 @@ function game() {
   const board = createBoard();
   const newBoard = createBoard();
   populateRandomBoard(board, board.percentage);
-  // populateCrazyBoard(board);
+  // populateAcornBoard(board);
 
 
   renderBoard(board);
